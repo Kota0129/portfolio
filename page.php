dae205fe9dababb5751e033c26cc509ffbad01ef
Kota <kota0129.office@gmail.com>
2025-08-15 14:58:35 +0900

    Update page.php
    スクリーンリーダー追記

--- page.php
+++ page.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 
-<main id="main" class="main-page">
-    <section class="page">
+<main id="main" class="main-page" role="main" tabindex="-1">
+    <section class="page" role="region" aria-labelledby="page-title">
         <div class="container">
-            <h1 class="page__title"><?php the_title(); ?></h1>
+            <h1 class="page__title" id="page-title"><?php the_title(); ?></h1>
 
             <div class="page__content">
                 <?php
